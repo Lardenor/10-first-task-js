17777279bd06db735d985336c5a00b7b9fc13e1a
Lardenor <yacmander@gmail.com>
2023-01-30 21:46:00 +0200

    first

---
+++ scrip.js
@@ -0,0 +1,81 @@
+// *
+// todo
+// ?
+// !
+// 1
+// let userName = prompt("Enter your name: ");
+// alert ("Hello "+ userName);
+
+
+//*2
+// let date = +prompt("Enter year of birthday ");
+// const nowYear = 2023;
+// let year = nowYear - date; 
+// alert ("You have "+year+" age");
+
+
+
+// !3
+// let length = +prompt("Enter size length :");
+// let perimeter = length * 4;
+// alert ("Square have P = "+ perimeter);
+
+
+// ? 4
+// let circle = +prompt("Enter radius of circle: ") ;
+// let squareCircle = Math.PI * (circle ** 2);
+// alert ("We have radius of circle: " + Math.round(squareCircle));
+
+
+
+// todo 5
+// let distance = +prompt("Enter distance in km between two city: ");
+// let time = +prompt("Enter time what you need: ");
+// let speed = distance / time;
+// alert("You need move with this speed: "+ speed+ " km/h");
+
+
+
+// * 6
+// let dollars = +prompt("Введіть скільки доларів у вас є ");
+// const euro = 1.09;
+// let convert = dollars / euro;
+// alert("You get: "+convert);
+
+// !7
+// let sizeGb = +prompt("Введіть місткість флеш накопичувача: " );
+
+// const sizeFile= 820;
+// let convertGb = sizeGb * 1024 ;
+// let ammountFile = convertGb/sizeFile;
+// alert("Ви можете загрузити: "+ Math.trunc(ammountFile)+ " кількість файлів.")
+
+
+
+// ?8
+// let ammountMoney = +prompt("Введіть суму грошей у вас наявну: " );
+// let priceChoco = +prompt("Введіть ціну на шоколад: " );
+// let ammountChoco;
+// let ammountRest;
+// if (priceChoco <= ammountMoney){
+
+//     ammountChoco = Math.floor(ammountMoney / priceChoco);
+//     ammountRest = ammountMoney-(ammountChoco * priceChoco);
+//     alert("Ви можете купити: "+ Math.trunc(ammountChoco)+ " шоколаду." + " Ваша решта: "+ ammountRest +" грн");
+// }
+// else{
+// alert("Сума грошей на вашому рахунку недостатня, іди працюй")
+// }
+
+
+// todo 9
+// let num = +prompt("Enter of third-number: ")
+// if (num>99 && num<1000 ){
+// let firstNumber = Math.trunc(num%100/10);
+// let secondNumber = Math.trunc(num%1000/100);
+// alert(num.toString()+firstNumber.toString()+secondNumber.toString());
+// } else {alert("It`s not a third-number");}
+// * 10
+// let number= +prompt("Введіть ціле число: ");
+// let result= (number%2 === 0)?"парне":"непарне";
+// alert("Число: "+result);
